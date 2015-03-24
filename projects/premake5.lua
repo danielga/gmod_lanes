58706c3a1ac9f00c3188ccbd9f5defa138240673
Daniel <danielga@outlook.pt>
2015-03-24 20:13:47 +0000

    Lowercased filepaths. Updated gmod-module-base submodule.

--- projects/premake5.lua
+++ projects/premake5.lua
@@ -1,5 +1,5 @@
 GARRYSMOD_MODULE_BASE_FOLDER = "../gmod-module-base"
-SOURCE_FOLDER = "../Source"
+SOURCE_FOLDER = "../source"
 PROJECT_FOLDER = os.get() .. "/" .. _ACTION
 
 solution("gm_lanes_core")
@@ -16,13 +16,13 @@ solution("gm_lanes_core")
 	filter("configurations:Release")
 		optimize("On")
 		vectorextensions("SSE2")
-		objdir(PROJECT_FOLDER .. "/Intermediate")
-		targetdir(PROJECT_FOLDER .. "/Release")
+		objdir(PROJECT_FOLDER .. "/intermediate")
+		targetdir(PROJECT_FOLDER .. "/release")
 
-	filter({"configurations:Debug"})
+	filter("configurations:Debug")
 		flags({"Symbols"})
-		objdir(PROJECT_FOLDER .. "/Intermediate")
-		targetdir(PROJECT_FOLDER .. "/Debug")
+		objdir(PROJECT_FOLDER .. "/intermediate")
+		targetdir(PROJECT_FOLDER .. "/debug")
 
 	project("gmsv_lanes_core")
 		kind("SharedLib")
