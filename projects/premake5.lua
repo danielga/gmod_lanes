c590ec282e45e784e559cd25a290f18e02be1780
Daniel <danielga@outlook.pt>
2015-05-15 15:05:05 +0100

    Converted to garrysmod_common format (smaller and easier premake files, too bad most of the size is for the garrysmod_common initialization). Removed gmod-module-base submodule. .gitignore now ignores everything (changes to premake5.lua or .gitignore don't bother us with pending changes on custom setups).

--- projects/premake5.lua
+++ projects/premake5.lua
@@ -1,97 +1,19 @@
-GARRYSMOD_MODULE_BASE_FOLDER = "../gmod-module-base"
-SOURCE_FOLDER = "../source"
-PROJECT_FOLDER = os.get() .. "/" .. _ACTION
+newoption({
+	trigger = "gmcommon",
+	description = "Sets the path to the garrysmod_common (https://bitbucket.org/danielga/garrysmod_common) directory",
+	value = "path to garrysmod_common dir"
+})
 
-solution("gm_lanes_core")
-	language("C++")
-	location(PROJECT_FOLDER)
-	warnings("Extra")
-	flags({"NoPCH", "StaticRuntime"})
-	platforms({"x86"})
-	configurations({"Release", "Debug"})
+local gmcommon = _OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON")
+if gmcommon == nil then
+	error("you didn't provide a path to your garrysmod_common (https://bitbucket.org/danielga/garrysmod_common) directory")
+end
 
-	filter("platforms:x86")
-		architecture("x32")
+include(gmcommon)
 
-	filter("configurations:Release")
-		optimize("On")
-		vectorextensions("SSE2")
-		objdir(PROJECT_FOLDER .. "/intermediate")
-		targetdir(PROJECT_FOLDER .. "/release")
+CreateSolution("lanes")
+	CreateProject(SERVERSIDE)
+		IncludeLuaShared()
 
-	filter("configurations:Debug")
-		flags({"Symbols"})
-		objdir(PROJECT_FOLDER .. "/intermediate")
-		targetdir(PROJECT_FOLDER .. "/debug")
-
-	project("gmsv_lanes_core")
-		kind("SharedLib")
-		defines({"GMMODULE", "LANES_SERVER"})
-		includedirs({
-			SOURCE_FOLDER,
-			GARRYSMOD_MODULE_BASE_FOLDER .. "/include"
-		})
-		files({SOURCE_FOLDER .. "/*.h", SOURCE_FOLDER .. "/*.c", SOURCE_FOLDER .. "/*.cpp"})
-		vpaths({
-			["Header files"] = SOURCE_FOLDER .. "/**.h",
-			["Source files"] = {
-				SOURCE_FOLDER .. "/**.cpp",
-				SOURCE_FOLDER .. "/**.c",
-			}
-		})
-
-		targetprefix("")
-		targetextension(".dll")
-
-		filter("system:windows")
-			libdirs({GARRYSMOD_MODULE_BASE_FOLDER})
-			links({"lua_shared"})
-			targetsuffix("_win32")
-
-		filter("system:linux")
-			defines({"POSIX", "GNUC", "_LINUX"})
-			linkoptions({"-Wl,-rpath,garrysmod/bin", "-l:lua_shared_srv.so"})
-			buildoptions({"-std=c++11"})
-			targetsuffix("_linux")
-
-		filter({"system:macosx"})
-			libdirs({GARRYSMOD_MODULE_BASE_FOLDER})
-			links({"lua_shared"})
-			buildoptions({"-std=c++11"})
-			targetsuffix("_mac")
-
-	project("gmcl_lanes_core")
-		kind("SharedLib")
-		defines({"GMMODULE", "LANES_CLIENT"})
-		includedirs({
-			SOURCE_FOLDER,
-			GARRYSMOD_MODULE_BASE_FOLDER .. "/include"
-		})
-		files({SOURCE_FOLDER .. "/*.h", SOURCE_FOLDER .. "/*.c", SOURCE_FOLDER .. "/*.cpp"})
-		vpaths({
-			["Header files"] = SOURCE_FOLDER .. "/**.h",
-			["Source files"] = {
-				SOURCE_FOLDER .. "/**.cpp",
-				SOURCE_FOLDER .. "/**.c",
-			}
-		})
-
-		targetprefix("")
-		targetextension(".dll")
-
-		filter("system:windows")
-			libdirs({GARRYSMOD_MODULE_BASE_FOLDER})
-			links({"lua_shared"})
-			targetsuffix("_win32")
-
-		filter("system:linux")
-			defines({"POSIX", "GNUC", "_LINUX"})
-			linkoptions({"-Wl,-rpath,garrysmod/bin", "-l:lua_shared.so"})
-			buildoptions({"-std=c++11"})
-			targetsuffix("_linux")
-
-		filter({"system:macosx"})
-			libdirs({GARRYSMOD_MODULE_BASE_FOLDER})
-			links({"lua_shared"})
-			buildoptions({"-std=c++11"})
-			targetsuffix("_mac")
+	CreateProject(CLIENTSIDE)
+		IncludeLuaShared()
